37cb8544000b726439aae3c64b2596d8ee2b8059
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-06-29 09:37:38 +0530

    payment Service

--- Booking-Service/src/main/java/com/MD/Booking/Service/controller/BookingController.java
+++ Booking-Service/src/main/java/com/MD/Booking/Service/controller/BookingController.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping({"/api/bookings"})
 @RequiredArgsConstructor
 public class BookingController {
 
